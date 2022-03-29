ac4b5f75863b530269d904defdcf71faa26e1b4a
tadas <tdavidsonas88@gmail.com>
2022-03-29 06:22:33 +0300

    Constructor property promotion example.

---
+++ constructor_property_promotion.php
@@ -0,0 +1,29 @@
+<?php
+
+class User
+{
+    public function __construct(protected $name)
+    {
+    }
+}
+
+class Plan
+{
+    public function __construct(protected string $name = 'monthly')
+    {
+    }
+}
+
+class Signup
+{
+    public function __construct(protected User $user, protected Plan $plan)
+    {
+    }
+}
+
+$user = new User('john_doe');
+$plan = new Plan();
+
+$signup = new Signup($user, $plan);
+
+var_dump($signup);
